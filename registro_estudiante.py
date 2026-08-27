9323d0444d77ae3c58c2686f54027dea44718d1b
60523701-alt <60523701@institutourusayhua.edu.pe>
2026-08-27 11:32:14 -0500

    primer cambio
    se agrego el primer cambio

---
+++ registro_estudiante.py
@@ -0,0 +1,19 @@
+#Mostrar un mensage en pantalla
+print("Sistema de registro de estudiantes")
+
+#Mostrar datos basicos de un estudiantes
+nombre = input("Ingrese su nombre: ")
+edad = int(input("ingrese su edad: "))
+Carrera = input("ingrese su careera: ")
+Semestre = input("ingrese su semestre: ")
+
+print("Nombre: ", nombre)
+print("Edad: ", edad)
+print("Carrera: ", Carrera)
+print("Semestre: ", Semestre)
+
+#mostrar el tipo de variable
+print(type(nombre))
+print(type(edad))
+print(type(Carrera))
+print(type(Semestre))
